8beedc2b2e1c401c29d3cf9bab3140ba50fa356a
shriyans009 <shriyanssahoo2005@gmail.com>
2025-04-13 20:08:10 +0530

    Created the structure

---
+++ src/main/java/com/travelguide/exceptions/NoRecommendationFoundException.java
@@ -0,0 +1,5 @@
+package src.main.java.com.travelguide.exceptions;
+
+public class NoRecommendationFoundException {
+    
+}
---
+++ src/main/java/com/travelguide/util/InputHandler.java
@@ -0,0 +1,5 @@
+package src.main.java.com.travelguide.util;
+
+public class InputHandler {
+    
+}
