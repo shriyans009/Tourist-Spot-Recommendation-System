be75c0fccac237a1c64fcf3676d4d8e6f6f3e9ec
shriyans009 <shriyanssahoo2005@gmail.com>
2025-04-13 20:38:52 +0530

    updated controller

--- src/main/java/com/travelguide/controller/RecommendationController.java
+++ src/main/java/com/travelguide/controller/RecommendationController.java
@@ -1,5 +1,17 @@
-package src.main.java.com.travelguide.controller;
+package com.travelguide.controller;
+
+import com.travelguide.service.RecommendationEngine;
+import com.travelguide.util.InputHandler;
+
+import java.util.Scanner;
 
 public class RecommendationController {
-    
+    public static void startApp() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(\"Enter city name to get recommendations:\");
+        String city = scanner.nextLine();
+
+        RecommendationEngine engine = new RecommendationEngine();
+        engine.recommendSpots(city);
+    }
 }
